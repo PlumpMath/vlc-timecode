66190640ef48d36c3a8fc856b56dbe9525a93b85
modo-lv <modo@modo.lv>
2017-04-25 22:59:33 +0300

    Basic functionality.

---
+++ VlcTimecode/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using FMUtils.KeyboardHook;
+
+namespace VlcTimecode {
+	public partial class Form1 : Form
+	{
+		private Boolean triggered = false;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			// Everytime a key is press we want to display it in a TextBox
+			var KeyboardHook = new Hook("Global Action Hook");
+			KeyboardHook.KeyDownEvent += (args) =>
+			{
+				if (args.Key == Keys.Q && args.isCtrlPressed && args.isAltPressed)
+				{
+					Task.Factory.StartNew(
+						() =>
+						{
+							Debug.WriteLine("Waiting for release...");
+							while (Control.ModifierKeys != 0)
+							{
+								// wait;
+							}
+							Debug.WriteLine("Released, go!");
+							TimeCoder.GetTimecode();
+						});
+				}
+			};
+		}
+
+		private void button1_Click(Object sender, EventArgs e)
+		{
+			this.textBox1.Focus();
+			this.textBox1.SelectAll();
+			TimeCoder.GetTimecode();
+		}
+	}
+}
---
+++ VlcTimecode/TimeCoder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace VlcTimecode {
+	public class TimeCoder {
+		public static void GetTimecode()
+		{
+			using (var client = new WebClient())
+			{
+				String credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(":pass"));
+				client.Headers[HttpRequestHeader.Authorization] = $"Basic {credentials}";
+				var xml = client.DownloadString(new Uri("http://localhost:8080/requests/status.xml"));
+				
+				var doc = new XmlDocument();
+				doc.LoadXml(xml);
+
+				XmlNode node = doc.SelectSingleNode("//time");
+				
+				var seconds = Int32.Parse(node.InnerText);
+				var time = new TimeSpan(0, 0, seconds);
+				var result = time.ToString(@"hh\:mm\:ss");
+
+				SendKeys.SendWait(result);
+			}
+		}
+	}
+}
